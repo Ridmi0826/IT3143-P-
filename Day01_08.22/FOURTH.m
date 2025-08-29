ec6e4a13ffb0255540f5a1da1b643a819b99be21
Ridmi Samarasundara <ridmisamarasundara@gmail.com>
2025-08-29 17:18:34 +0530

    Update FOURTH.m

--- Day01_08.22/FOURTH.m
+++ Day01_08.22/FOURTH.m
@@ -5,5 +5,4 @@ imshow(A);
 subplot(1,2,2)
 imagesc(A);
 colormap(gca,'jet');%gca-get current axis(current subplot)
-%palete should be beloow from the imagesc if not it will not show the color of
-%the color plalete
+%pallete should be below from the imagesc if not it will not show the color of the color pallete
