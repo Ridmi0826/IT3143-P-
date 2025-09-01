c702f7c571bd6b6fd8b96e68bdd25aaf44cc8098
Ridmi Samarasundara <ridmisamarasundara@gmail.com>
2025-09-01 13:25:26 +0530

    Update prg4.m

--- Day02_08.29/prg4.m
+++ Day02_08.29/prg4.m
@@ -1,7 +1,9 @@
-B=rand(256).*1000;   %Generate random image array
+B = rand(256) .* 1000;       % Generate a 256x256 matrix with random values scaled up to 1000
+                             % This simulates an image with pixel intensity values ranging from 0 to 1000
 
-%imshow(B);         %poor cintrast resullts using image
-                   %exceeds expected range
+% imshow(B);                 % Displays the image, but results in poor contrast
+                             % because imshow assumes default display range [0, 1] for double arrays
 
-imshow(B,[0 1000]);  %But if we specify range of dat
-                     %imshow also displays correct
+imshow(B, [0 1000]);         % Displays the image with correct contrast
+                             % by explicitly specifying the intensity range from 0 to 1000
+                             % This ensures full dynamic range is used for visualization
